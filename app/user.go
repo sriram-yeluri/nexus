db11056f2e88eef0e199bc5ca3b9124679f84d89
Allan Tony Selvan <allan.tony.selvan@nl.abnamro.com>
2022-05-06 16:45:13 +0200

    updated get users list code

--- app/user.go
+++ app/user.go
@@ -1,19 +1,47 @@
 package app
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"github.com/go-resty/resty/v2"
+	"net/http"
 )
 
-func GetUserList() {
+type User struct {
+	UserId        string        `json:"userId"`
+	FirstName     string        `json:"firstName"`
+	LastName      string        `json:"lastName"`
+	EmailAddress  string        `json:"emailAddress"`
+	Source        string        `json:"source"`
+	Status        string        `json:"status"`
+	ReadOnly      bool          `json:"readOnly"`
+	Roles         []string      `json:"roles"`
+	ExternalRoles []interface{} `json:"externalRoles"`
+}
 
-	endpoint := fmt.Sprintf("%s/service/rest/v1/security/users", NXRMConfig.URL)
-	InfoLogger.Println(endpoint)
-	resp, err := client.R().Get(endpoint)
-	if err != nil {
-		ErrorLogger.Println(err)
-	} else {
-		log.Println(string(resp.Body()))
+type UsersManager struct {
+	Client *resty.Client
+}
+
+func NewUsersManager(client *resty.Client) UsersManager {
+	return UsersManager{
+		client,
 	}
+}
 
+func (um *UsersManager) GetUsers() (*[]User, error) {
+	users := new([]User)
+	uri := fmt.Sprintf("%s/service/rest/v1/security/users", NXRMConfig.URL)
+	InfoLogger.Println(uri)
+	resp, err := client.R().SetResult(users).Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	switch resp.StatusCode() {
+
+	case http.StatusOK:
+		return users, nil
+	default:
+		return nil, errors.New("request to nexus failed...")
+	}
 }
--- cmd/list.go
+++ cmd/list.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
 	"github.com/sriram-yeluri/nxrm3/app"
 )
@@ -15,7 +17,13 @@ var listCmd = &cobra.Command{
 	Short: "Get list of users in Nexus",
 	Long:  `Get list of users in Nexus`,
 	Run: func(cmd *cobra.Command, args []string) {
-		app.GetUserList()
+		um := app.NewUsersManager(resty.New())
+		users, err := um.GetUsers()
+		if err != nil {
+			app.ErrorLogger.Println(err)
+			return
+		}
+		fmt.Println(*users)
 	},
 }
 
